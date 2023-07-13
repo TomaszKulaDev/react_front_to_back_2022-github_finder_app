4e63fde3e9659e651da88e9089c4c234facdcc78
Tomasz Kula <tomaszkulewicz@gmail.com>
2023-07-13 13:51:27 +0200

    50. Fetching users with env and setting list od users.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from "../components/layout/Navbar";
 import Footer from "../components/layout/Footer";
+import UserResults from "../components/users/UserResults";
 
 const Home = () => {
 
@@ -8,7 +9,8 @@ const Home = () => {
         <div className='flex flex-col justify-between h-screen'>
             <Navbar title='GitHub Finder'/>
                 <main className='container mx-auto px-3 pb-12'>
-                    <h1 className='text-6xl'> Welcome </h1>
+                    <h1 className='text-6xl mb-8'> Welcome </h1>
+                    <UserResults/>
                 </main>
             <Footer/>
         </div>
