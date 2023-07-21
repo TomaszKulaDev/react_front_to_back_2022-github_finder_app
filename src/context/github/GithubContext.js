6f8ade75751c95c21ece5e18ecdfe8a30a59065d
Tomasz Kula <tomaszkulewicz@gmail.com>
2023-07-21 22:19:06 +0200

    54. Added Reducers

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
-import {createContext, useState} from "react";
+import {createContext, useReducer} from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext()
 
@@ -7,8 +8,12 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 
 export const GithubProvider = ({children}) => {
 
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const initialState = {
+        users: [],
+        loading: true
+    }
+
+    const [state, dispatch] = useReducer(githubReducer, initialState)
 
     const fetchUsers = async () => {
         const response = await fetch(`${GITHUB_URL}/users`, {
@@ -18,19 +23,22 @@ export const GithubProvider = ({children}) => {
         })
         const data = await response.json()
 
-        setUsers(data)
-        setLoading(false)
+        console.log(data)
+        dispatch({
+            type: 'GET_USERS',
+            payload: data,
+        })
     }
 
-    return <GithubContext.Provider value={{
-        users,
-        loading,
-        fetchUsers
-    }}>
+    return <GithubContext.Provider
+        value={{
+            users: state.users,
+            loading: state.loading,
+            fetchUsers
+        }}>
 
         {children}
     </GithubContext.Provider>
-
 }
 
 export default GithubContext
