9503ce41122cdb773e29a483da3793996aea84cd
Tomasz Kula <tomaszkulewicz@gmail.com>
2023-07-26 21:29:51 +0200

    59. Setting Alerts context reducer

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import {GithubProvider} from "./context/github/GithubContext";
+import {AlertProvider} from "./context/alert/AlertContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <GithubProvider>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </GithubProvider>
+        <AlertProvider>
+            <GithubProvider>
+                <BrowserRouter>
+                    <App/>
+                </BrowserRouter>
+            </GithubProvider>
+        </AlertProvider>
     </React.StrictMode>
 );
 
